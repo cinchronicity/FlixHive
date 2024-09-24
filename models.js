537813c91114c07b6be305b39f7c2c2dde4b85ad
cinchronicity <168139730+cinchronicity@users.noreply.github.com>
2024-09-23 23:00:17 -0700

    added actor model /schema

--- models.js
+++ models.js
@@ -1,31 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-let movieSchema = mongoose.Schema({ // Schema for movies
-    Title: {type: String, required: true},
-    Description: {type: String, required: true},
-    Genre: {
-      Name: String,
-      Description: String
-    },
-    Director: {
-      Name: String,
-      Bio: String
-    },
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
-  });
-  
-  let userSchema = mongoose.Schema({ //defining the schema for users
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-  });
-  
-  let Movie = mongoose.model('Movie', movieSchema); //creates the model for movies that use the movieSchema
-  let User = mongoose.model('User', userSchema);
-  
-  module.exports.Movie = Movie;
-  module.exports.User = User;
+let movieSchema = mongoose.Schema({
+  // Schema for movies
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: {
+    name: String,
+    description: String,
+  },
+  director: {
+    name: String,
+    bio: String,
+  },
+  actors: [{ type: mongoose.Schema.Types.ObjectId, ref: "Actor" }], //array of actor objects
+  ImagePath: String,
+  Featured: Boolean,
+});
+
+let userSchema = mongoose.Schema({
+  //defining the schema for users
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  Birthday: Date,
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }], //
+});
+
+let actorSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  birthYear: { type: Number, required: true },
+});
+
+let Movie = mongoose.model("Movie", movieSchema); //creates the model for movies that use the movieSchema
+let User = mongoose.model("User", userSchema);
+let Actor = mongoose.model("Actor", actorSchema); //creates the model for actors
+
+module.exports.Movie = Movie; //allows us to import the Movie model to other files
+module.exports.User = User;
+module.exports.Actor = Actor; //export the Actor model
