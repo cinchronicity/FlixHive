ab10bceec7f3a55416edd709acfddadcb593cac0
Cindy <168139730+cinchronicity@users.noreply.github.com>
2025-08-06 15:18:26 -0700

    Update CORS to allow all origins

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ app.use(bodyParser.json()); //lets us to be able to read data from body object
 app.use(express.urlencoded({ extended: true })); //allows us to read data from the body of POST requests
 
 const cors = require("cors"); //import cors module to allow requests from all origins
-let allowedOrigins = [
-  "http://localhost:8080",
-  "http://testsite.com",
-  "http://localhost:1234",
-  "https://flixhive-cf7fbbd939d2.herokuapp.com",
-  "https://the-flixhive.netlify.app", 
-  "http://localhost:4200"
-];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// let allowedOrigins = [
+//   "http://localhost:8080",
+//   "http://testsite.com",
+//   "http://localhost:1234",
+//   "https://flixhive-cf7fbbd939d2.herokuapp.com",
+//   "https://the-flixhive.netlify.app", 
+//   "http://localhost:4200"
+// ];
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn’t allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
-// app.use(cors());  //allows all domains to access the API
+app.use(cors());  //allows all domains to access the API
 
 //passport middleware function for authenticating users
 let auth = require("./auth")(app); //(app) allows Express into auth.js file
